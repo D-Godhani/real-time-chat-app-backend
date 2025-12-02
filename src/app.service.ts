c635653ab8c004d6f4d473cd0efb6799be8006c5
Dharmik Godhani <122303920+D-Godhani@users.noreply.github.com>
2025-12-02 21:53:08 +0530

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! Backend is Running';
+    return 'Hello World! Backend is Running!';
   }
 }
